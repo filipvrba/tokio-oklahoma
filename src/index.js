59ed587143b0d68f9ee330e0d1553bce1a20a8cb
Filip Vrba <filipvrba@pm.me>
2021-11-03 21:42:57 +0100

    resize canvas element & rendering

--- src/index.js
+++ src/index.js
@@ -31,9 +31,22 @@ class MyGame extends Phaser.Scene
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: MyGame
 };
 
 const game = new Phaser.Game(config);
+
+function resize() {
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    game.canvas.width = width;
+    game.canvas.height = height;
+
+    game.renderer.resize( width, height );
+}
+
+window.onresize = () => resize();
