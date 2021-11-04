0a119877589b5fc26869a2ddf2b3d75d7b05f428
Filip Vrba <filipvrba@pm.me>
2021-11-04 22:57:42 +0100

    size is fixed

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import Phaser from 'phaser';
 import * as levels from './scenes/levels.js';
 
-const config = {
-    type: Phaser.WEBGL,
-    parent: 'phaser-example',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-        default: 'arcade'
-    },
-    scene: levels.Main
-};
+function initGame() {
 
-const game = new Phaser.Game(config);
+    const fullHD = new Phaser.Math.Vector2( 1920, 1080 );
 
-function resize() {
+    const config = {
+        type: Phaser.AUTO,
+        width: fullHD.y,
+        height: fullHD.x,
+        scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
+		},
+        physics: {
+            default: 'arcade'
+        },
+        scene: levels.Main
+    };
+    
+    const game = new Phaser.Game(config);
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    game.canvas.width = width;
-    game.canvas.height = height;
-
-    game.renderer.resize( width, height );
-    game.scale.resize( width, height );
 }
 
-const resizeHandler = () => resize();
-window.onresize = resizeHandler;
+window.onload = () => {
 
-screen.orientation.onchange = resizeHandler;
+    initGame();
+
+}
